e6f2cf137a416bc487e9f43ebb5d9c387b09cbc0
Juan <juan000martinez000@gmail.com>
2024-09-10 19:49:52 -0300

    Pantalla de formulario de pasajeros completa

--- app/src/main/java/com/example/vanalaeropuerto/fragments/IngresoDatosFragment.kt
+++ app/src/main/java/com/example/vanalaeropuerto/fragments/IngresoDatosFragment.kt
@@ -6,21 +6,49 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.LinearLayout
+import android.widget.ProgressBar
 import android.widget.RadioButton
+import androidx.lifecycle.Observer
 import com.example.vanalaeropuerto.R
+import com.example.vanalaeropuerto.data.ViewState
 import com.example.vanalaeropuerto.viewmodels.IngresoDatosViewModel
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 class IngresoDatosFragment : Fragment() {
 
     private lateinit var v : View
+    private lateinit var viewModel: IngresoDatosViewModel
+
+    private lateinit var progressBar : ProgressBar
 
     private lateinit var rbThirdParty : RadioButton
     private lateinit var rbMyself : RadioButton
     private lateinit var thirdPartyFields : LinearLayout
+    private lateinit var fabConfirmForm : FloatingActionButton
+    private lateinit var etUserName : EditText
+    private lateinit var etUserSurname : EditText
+    private lateinit var etUserPhoneNumber : EditText
+    private lateinit var etUserCuil : EditText
+    private lateinit var etThirdPartyName : EditText
+    private lateinit var etThirdPartySurname : EditText
+    private lateinit var etThirdPartyPhoneNumber : EditText
+    private lateinit var etThirdPartyCuil : EditText
 
+    private lateinit var userName : String
+    private lateinit var userSurname : String
+    private lateinit var userPhoneNumber : String
+    private lateinit var userCuil : String
 
-    private lateinit var viewModel: IngresoDatosViewModel
+    private lateinit var thirdPartyName : String
+    private lateinit var thirdPartySurname : String
+    private lateinit var thirdPartyPhoneNumber : String
+    private lateinit var thirdPartyCuil : String
+
+    private var radiobuttonChecked : Boolean = false
+    private var isForUser : Boolean = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,9 +56,21 @@ class IngresoDatosFragment : Fragment() {
     ): View? {
         v = inflater.inflate(R.layout.fragment_ingreso_datos, container, false)
 
+        progressBar = v.findViewById(R.id.progressBarLoading)
         rbThirdParty = v.findViewById(R.id.rb_third_party)
         rbMyself = v.findViewById(R.id.rb_myself)
         thirdPartyFields = v.findViewById(R.id.third_party_fields)
+        fabConfirmForm = v.findViewById(R.id.fab_confirm_form)
+
+        etUserName = v.findViewById(R.id.et_user_name)
+        etUserSurname = v.findViewById(R.id.et_user_surname)
+        etUserPhoneNumber = v.findViewById(R.id.et_user_phone_number)
+        etUserCuil = v.findViewById(R.id.et_user_cuil)
+
+        etThirdPartyName = v.findViewById(R.id.et_third_party_name)
+        etThirdPartySurname = v.findViewById(R.id.et_third_party_surname)
+        etThirdPartyPhoneNumber = v.findViewById(R.id.et_third_party_phone_number)
+        etThirdPartyCuil = v.findViewById(R.id.et_third_party_cuil)
 
 
         return v
@@ -43,15 +83,76 @@ class IngresoDatosFragment : Fragment() {
         rbThirdParty.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
                 thirdPartyFields.visibility = View.VISIBLE
+                radiobuttonChecked = true
+                isForUser = false
             }
         }
 
         rbMyself.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
                 thirdPartyFields.visibility = View.GONE
+                radiobuttonChecked = true
+                isForUser = true
             }
         }
 
+        fabConfirmForm.setOnClickListener {
+            userName = etUserName.text.toString()
+            userSurname = etUserSurname.text.toString()
+            userPhoneNumber = etUserPhoneNumber.text.toString()
+            userCuil = etUserCuil.text.toString()
+
+            thirdPartyName = etThirdPartyName.text.toString()
+            thirdPartySurname = etThirdPartySurname.text.toString()
+            thirdPartyPhoneNumber = etThirdPartyPhoneNumber.text.toString()
+            thirdPartyCuil = etThirdPartyCuil.text.toString()
+
+            viewModel.validateUserData(radiobuttonChecked,userName, userSurname, userPhoneNumber, userCuil)
+
+            if (!isForUser) {
+                viewModel.validateUserData(radiobuttonChecked,thirdPartyName, thirdPartySurname, thirdPartyPhoneNumber, thirdPartyCuil)
+            }
+
+            viewModel.viewState.observe(viewLifecycleOwner, Observer { viewState ->
+                when (viewState) {
+                    is ViewState.Loading -> {
+                        this.showLoading()
+                    }
+                    is ViewState.Failure -> {
+                        this.showError()
+                    }
+                    is ViewState.Idle -> {
+                        this.hideLoading()
+                    }
+                    is ViewState.InvalidParameters -> {
+                        this.showInvalidParameters()
+                    }
+                    else -> {
+                        this.showError()
+                    }
+                }
+            })
+
+        }
+
     }
+
+    private fun showLoading() {
+        progressBar.visibility = View.VISIBLE
+    }
+
+    private fun hideLoading() {
+        progressBar.visibility = View.GONE
+    }
+
+    private fun showError() {
+        progressBar.visibility = View.GONE
+        Snackbar.make(v, getString(R.string.ha_ocurrido_un_error), Snackbar.LENGTH_SHORT).show()
+    }
+    private fun showInvalidParameters() {
+        progressBar.visibility = View.GONE
+        Snackbar.make(v, getString(R.string.invalid_parameters), Snackbar.LENGTH_SHORT).show()
+    }
+
 
 }
--- app/src/main/java/com/example/vanalaeropuerto/viewmodels/IngresoDatosViewModel.kt
+++ app/src/main/java/com/example/vanalaeropuerto/viewmodels/IngresoDatosViewModel.kt
@@ -1,7 +1,69 @@
 package com.example.vanalaeropuerto.viewmodels
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.vanalaeropuerto.data.ViewState
+import java.util.Calendar
 
 class IngresoDatosViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val _viewState = MutableLiveData<ViewState>()
+    val viewState: LiveData<ViewState> get() = _viewState
+
+    fun validateUserData(
+        radiobuttonChecked: Boolean,
+        name: String?,
+        surname: String?,
+        phoneNumber: String?,
+        cuil: String?
+    ) {
+        _viewState.value = ViewState.Loading
+
+        val currentDateInMillis = Calendar.getInstance().timeInMillis
+        val errores = mutableListOf<String>()
+
+        // Validación de radiobutton
+        if (!radiobuttonChecked) {
+            errores.add("Debe seleccionar para quién corresponde el viaje")
+        }
+
+        // Validación de nombre
+        if (name.isNullOrBlank()) {
+            errores.add("Nombre no puede estar vacío")
+        } else if (!name.matches(Regex("^[a-zA-ZáéíóúÁÉÍÓÚñÑ\\s]{2,50}\$"))) {
+            errores.add("Nombre no es válido. Solo se permiten letras y debe tener entre 2 y 50 caracteres.")
+        }
+
+        // Validación de apellido
+        if (surname.isNullOrBlank()) {
+            errores.add("Apellido no puede estar vacío")
+        } else if (!surname.matches(Regex("^[a-zA-ZáéíóúÁÉÍÓÚñÑ\\s]{2,50}\$"))) {
+            errores.add("Apellido no es válido. Solo se permiten letras y debe tener entre 2 y 50 caracteres.")
+        }
+
+        // Validación de telefono
+        if (phoneNumber.isNullOrBlank()) {
+            errores.add("Telefono no puede estar vacía")
+        } else if (!phoneNumber.matches(Regex("^[+]?[0-9]{10,13}\$"))) {
+            errores.add("Teléfono no es válido. Debe contener entre 10 y 13 dígitos, y puede incluir un '+' al inicio.")
+        }
+
+        // Validación de cuil
+        if (cuil.isNullOrBlank()) {
+            errores.add("Cuil no puede estar vacía")
+        } else if (!cuil.matches(Regex("^\\d{2}\\d{8}\\d{1}\$"))) {
+            errores.add("CUIL no es válido. Debe tener el formato XX-XXXXXXXX-X.")
+        }
+
+
+        // Manejo de errores
+        if (errores.isNotEmpty()) {
+            _viewState.value = ViewState.InvalidParameters
+            Log.e("ValidarDatos", "Errores: ${errores.joinToString(", ")}")
+        } else {
+            _viewState.value = ViewState.Idle
+        }
+    }
 }
